fc33e494bea6618bececfb1837a5732a9571ad63
Monesillo <162189011+Monesillo@users.noreply.github.com>
2024-04-10 23:46:20 +0000

    javascript excuse

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello, I'm excuse generator!");
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+function getExcuseGen() {
+  let changeWho = who[Math.floor(Math.random() * who.length)];
+  let changeAction = action[Math.floor(Math.random() * action.length)];
+  let changeWhat = what[Math.floor(Math.random() * what.length)];
+  let changeWhen = when[Math.floor(Math.random() * when.length)];
+
+  return changeWho + " " + changeAction + " " + changeWhat + " " + changeWhen;
+}
+
+document.getElementById("excuse").innerHTML = getExcuseGen();
